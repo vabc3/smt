43416d858db917a4959f877cb0f21d6687eb4fcf
Karata <vabc3h@gmail.com>
2015-08-03 21:07:25 +0800

    Add lib files

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 TARGETS	= run
 CC		= armv6j-hardfloat-linux-gnueabi-gcc
+CFLAGS	= -Ilibpcd
+LDFLAGS	= -lpthread
+# precompiled static lib
+LIBWP	= ../wiringPi/wiringPi/libwiringPi.a
+# target machine location
+TM		= root@pi
 
-.PHONY : all clean deploy
+.PHONY : all clean deploy $(LIBWP)
 
 all     : $(TARGETS)
 
@@ -14,4 +20,7 @@ deploy  : all
 test	: deploy
 	ssh $(TM) deploy/run
 
-run     : run.o
+run     : run.o PCD8544.o $(LIBWP)
+
+PCD8544.o : libpcd/PCD8544.c
+	$(CC) $(CFLAGS) -c $< -o $@ 
--- run.c
+++ run.c
@@ -1,10 +1,38 @@
+#include "wiringPi.h"
+#include "PCD8544.h"
+#include "zbd.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sysinfo.h>
 
-int main()
+// pin setup
+int _din = 1;
+int _sclk = 0;
+int _dc = 2;
+int _rst = 4;
+int _cs = 3;
+int contrast = 45;
+
+int main ()
 {
-	int q;
-	scanf("%d",&q);
-	printf("ho~%dx--\n",q+1);
-	
-	return 0;
+  // check wiringPi setup
+  if (wiringPiSetup() == -1)
+  {
+    printf("wiringPi Error\n");
+    exit(1);
+  }
+  
+  // init and clear lcd
+  LCDInit(_sclk, _din, _dc, _cs, _rst, contrast);
+  LCDclear();
+
+  //LCDshowLogo();
+  LoadBuf(zbd);
+  delay(1000);
+  LCDclear();
+  LCDdisplay();
+
+  return 0;
 }
